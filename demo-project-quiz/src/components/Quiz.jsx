bf6ac129810e8a55c0a1c8dc0e0d4a345eb89eba
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-08-14 22:45:38 +0700

    Deriving Values, Outputting Questions & Registering Answers

--- demo-project-quiz/src/components/Quiz.jsx
+++ demo-project-quiz/src/components/Quiz.jsx
@@ -7,7 +7,26 @@ function Quiz() {
 
   const activeQuestionIndex = userAnswers.length;
 
-  return <div>quiz</div>;
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
