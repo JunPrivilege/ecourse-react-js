0b25bd5831ab3eaf76f88f103c1ac6aab6009af2
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-08-19 03:17:43 +0700

    Using Keys For Resetting Components & Add Some Caption

--- behind-scenes/src/App.jsx
+++ behind-scenes/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
 
   function handleSetCount(newCount) {
     setChosenCount(newCount);
+    setChosenCount((prevChosenCount) => prevChosenCount + 1) // Using Previous Value To Create New Value
   }
 
   return (
