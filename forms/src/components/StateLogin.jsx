1beb5503604edf765edf0a31d342a74259322d4b
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-08-28 19:10:26 +0700

    Managing & Getting User Input via State & Generic Handlers

---
+++ forms/src/components/StateLogin.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+export default function Login() {
+  // MANAGING & GETTING USER INPUT VIA STATE
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setenteredPassword] = useState("");
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  // }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setenteredPassword(event.target.value);
+  // }
+
+  // MANAGING & GETTING USER INPUT VIA GENERIC HANDLERS
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, event) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event)}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event)}
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
