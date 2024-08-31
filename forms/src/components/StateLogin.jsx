57fafbc57cebc4e78f2aea7acee858749bb2c4c4
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-08-31 17:45:02 +0700

    Validating Input Upon Lost Focus (Blur)

--- forms/src/components/StateLogin.jsx
+++ forms/src/components/StateLogin.jsx
@@ -23,8 +23,12 @@ export default function Login() {
     password: "",
   });
 
-  const emailIsInvalid =
-    enteredValues.email !== "" && !enteredValues.email.includes("@");
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -41,6 +45,18 @@ export default function Login() {
       ...prevValues,
       [identifier]: event.target.value,
     }));
+    
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
   }
 
   return (
@@ -54,6 +70,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event)}
             value={enteredValues.email}
           />
