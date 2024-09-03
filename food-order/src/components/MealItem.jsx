53074ccdeb04570d9922e39e7374577a3df82f3b
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-09-03 22:40:18 +0700

    Creating a Configurable & Flexible Custom Button Component

--- food-order/src/components/MealItem.jsx
+++ food-order/src/components/MealItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button.jsx";
 
 function MealItem({ meal }) {
   return (
@@ -15,7 +16,7 @@ function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
