6ed0d568dcca7c3ae6d6680b30b4681f454df1d7
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-09-22 21:24:14 +0700

    Finalizing the Fetching Logic

--- advanced-redux/src/store/cart-action.js
+++ advanced-redux/src/store/cart-action.js
@@ -47,7 +47,10 @@ export const sendCartData = (cart) => {
         "https://advanced-redux-11683-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
