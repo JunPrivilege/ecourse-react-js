a5070505713a65dcda63e2fe293f58631a32a736
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-10-02 09:07:22 +0700

    Where Should loader() Code Be Stored?

--- routing/frontend/src/components/EventsList.js
+++ routing/frontend/src/components/EventsList.js
@@ -1,13 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 
 import classes from "./EventsList.module.css";
 
-function EventsList() {
-  const events = useLoaderData();
+function EventsList({ events }) {
+  // const events = useLoaderData();
 
   return (
     <div className={classes.events}>
-      <h1>All Events</h1>
+      <h1>All Events</h1> 
       <ul className={classes.list}>
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
