b365c337507e509278ad3945a248715674541a4d
JunPrivilege <tifalrizkyarjuna@gmail.com>
2024-10-10 19:40:15 +0700

    Planning Data Submission

--- routing/frontend/src/pages/NewEventPage.js
+++ routing/frontend/src/pages/NewEventPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return <h1>NewEventPage</h1>;
+  return <EventForm />;
 }
 
 export default NewEventPage;
